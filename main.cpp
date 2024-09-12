7854109875ef6d129ef086c30152624a5edb6cd8
Luis Felipe Caraballo <luis.caraballo7@upr.edu>
2024-09-12 00:38:27 +0000

    tarea2

--- main.cpp
+++ main.cpp
@@ -3,20 +3,55 @@
 
 
 #include <iostream>
+#include <iomanip>
 using namespace std;
 
-int main(){
-    double name, adult, child;
+int main() {
+    //Display the purpose of this program
+    cout << "This program calculates a theater's gross and net box office profit." << endl;
 
-    cout << "Enter the name of the movie: ";
-    cinn >> name;
-    cout << "Enter the amount of adult tickets sold: ";
-    cinn >> adult
-    cout << "Enter the amount of child tickets sold: ";
-    cinn >> child
+    //Get movie name, adult tickets sold, and child tickets sold
+    string movieName;
+    int adultTickets, childTickets;
+    
+    cout << "Enter the movie name: ";
+    getline(cin, movieName);
 
-    //Calculate Gross Box ofice profit
-    Gross = (adult ticket x adult tichet sold + child ticket + child ticket sold)
+    cout << "Enter the number of adult tickets sold: ";
+    cin >> adultTickets;
+
+    cout << "Enter the number of child tickets sold: ";
+    cin >> childTickets;
+
+    //Calculate ticket sales
+    double adultTicketPrice = 10.00;
+    double childTicketPrice = 6.00;
+
+    double adultTicketSales = adultTickets * adultTicketPrice;
+    double childTicketSales = childTickets * childTicketPrice;
+
+    //Calculate gross box office profit
+    double grossBoxOfficeProfit = adultTicketSales + childTicketPrice;
+
+    //Calculate net box office profit (20% of gross)
+    double netBoxOfficeProfit = 0.20 * grossBoxOfficeProfit;
+
+    //Calculate amount paid to distributor
+    double amountPaidToDistributor= grossBoxOfficeProfit - netBoxOfficeProfit;
+
+    //Display report
+    cout << "\nMovie Name: " << setw(36) << movieName << endl;
+    cout << "Adult Tickets Sold: " << setw(20) << adultTickets << endl;
+    cout << "Child Tickets Sold: " << setw(20) << childTickets << endl;
+    cout << "Gross Box Office Profit: " <<setw(12) << fixed << setprecision (2) << "$" << grossBoxOfficeProfit << endl;
+    cout << "Net Box Office Profit: " <<setw(14) << fixed << setprecision(2) << "$" << netBoxOfficeProfit << endl;
+    cout << "Amount Paid to Distributor: " <<setw(9) << fixed << setprecision(2) << "$" << amountPaidToDistributor << endl;
+
+    return 0;
+}
+
+
+    
     
 
 
@@ -28,4 +63,4 @@ int main(){
 
 
 
-}
+
