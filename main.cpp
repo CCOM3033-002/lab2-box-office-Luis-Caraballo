98d6d580a6f3dc609396635a4deac19f563e6edd
Luis Felipe Caraballo <luis.caraballo7@upr.edu>
2024-09-12 00:18:43 +0000

    tarea2

--- main.cpp
+++ main.cpp
@@ -1,1 +1,31 @@
+//Luis Caraballo Aponte
+//This program calculates the Gross and Net Box Office Profit and the Distributer's Profit of a movvie laying in the theater
 
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    double name, adult, child;
+
+    cout << "Enter the name of the movie: ";
+    cinn >> name;
+    cout << "Enter the amount of adult tickets sold: ";
+    cinn >> adult
+    cout << "Enter the amount of child tickets sold: ";
+    cinn >> child
+
+    //Calculate Gross Box ofice profit
+    Gross = (adult ticket x adult tichet sold + child ticket + child ticket sold)
+    
+
+
+
+
+
+
+
+
+
+
+}
